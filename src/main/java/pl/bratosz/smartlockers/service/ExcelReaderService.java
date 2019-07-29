5f7dc0f7f71a6adc614c99f3bf11d11f7075c050
Bartosz Palej <palej.bartosz@gmail.com>
2019-07-29 19:53:32 +0200

    Excel reader beginning

---
+++ src/main/java/pl/bratosz/smartlockers/service/ExcelReaderService.java
@@ -0,0 +1,11 @@
+package pl.bratosz.smartlockers.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExcelReaderService {
+    public static final String XLSX_FILE_TO_READ = "C:/uploads.file.xlsx";
+
+    Workbook workbook
+}
