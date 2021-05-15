3ba6f53397114ab2a053169722daea697a273721
Bartosz Palej <palej.bartosz@gmail.com>
2021-05-15 16:20:49 +0200

    Cosmetic changes

--- src/main/resources/static/js/smart-lockers.js
+++ src/main/resources/static/js/smart-lockers.js
@@ -20,7 +20,7 @@ function getLockersByPlant(plantId) {
 }
 
 function getLockersByClient(clientId) {
-    return getActualLocation() + `/lockers/get-by-client/${clientId}`;
+    return getActualLocation() + `/lockers/get-by-client/git ${clientId}`;
 }
 
 function getLockersFiltered(plantId, departmentId, locationId) {
